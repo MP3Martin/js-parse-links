e1b72c9decb70b487cd51201ddefb052e91bd95e
MP3Martin <60501493+MP3Martin@users.noreply.github.com>
2022-05-21 23:07:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -41,9 +41,10 @@ $(document).ready(function() {
     var placeholders_list = par_placeholder.split(";")
     for(var placeholder1 in placeholders_list) {
         temp1 = placeholder1.split(",")
+        alert(temp1)
         one1 = temp1[0]
         two1 = temp1[1]
         placeholders[one1] = two1
     }
-    alert(placeholders)
+    //
 });
