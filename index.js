b6c3b006039015a924c61712032eb3ff511a5cf1
MP3Martin <60501493+MP3Martin@users.noreply.github.com>
2022-05-21 21:14:36 +0200

    .

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+// -- Setup variables -- \\
+let par_url = ""
+let par_placeholder = ""
+let par_type = ""
+let error_missing_pars = "There are missing required parameters: "
+let errored = 0
+
+
 function getPar(name, url) {
      if (!url) url = window.location.href;
      name = name.replace(/[\[\]]/g, "\\$&");
@@ -11,4 +19,24 @@ function getPar(name, url) {
 $(document).ready(function() {
     $("#error_text").hide()
     $("#test_text").text('The "a" parameter is ' + getPar("a").length + ' characters long (' + getPar("a") + ')');
+
+    // -- Get parameters -- \\
+    par_url = getPar("url");
+    par_placeholder = getPar("placeholder");
+    par_type = getPar("type");
+
+    required_placeholders = [par_url]
+
+    for ( let placeholder of required_placeholders ) {
+
+        if (placeholder == '') {
+            error_missing_pars += "<code>" + placeholder + "</code>" + ", "
+            errored = errored + 1
+        }
+    }
+
+    if (errored > 0) {
+        $("#error_text").show()
+        $("#error_text").html(error_missing_pars)
+    }
 });
