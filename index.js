4e76780e3716920aceaef2913e47c72f75848062
MP3Martin <60501493+MP3Martin@users.noreply.github.com>
2022-05-28 19:22:03 +0200

    .

--- index.js
+++ index.js
@@ -57,8 +57,9 @@ $(document).ready(function() {
             if (isUrl(two1.split(".")[0])) {
                 console.log(two1 + " is a url")
 
-                temp8928 = two1.split(".")
-                temp8928 = temp8928.shift()
+                temp8928 = two1 + ""
+                // temp8928 = temp8928.split(".")
+                // temp8928 = temp8928.shift()
                 // temp8928 = temp8928.join(".")
 
                 urls[two1.split(".")[0]] = temp8928
