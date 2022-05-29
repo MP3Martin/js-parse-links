acaaa952401c0dc46068a8c63b4a944d12714e91
MP3Martin <60501493+MP3Martin@users.noreply.github.com>
2022-05-29 14:18:59 +0200

    .

--- index.js
+++ index.js
@@ -146,7 +146,7 @@ $(document).ready(function() {
             errored = errored + 1
             $("#loading").hide()
             $("#error_text").show()
-            misc_error += "<code>" + error + "</code>" + "<br>" + "The browser likely can't reach one of the servers."
+            misc_error += "<code>" + error + "</code>" + "<br><br>" + "The browser likely can't reach one of the servers."
             $("#error_text").html(misc_error + "<br>")
         });
         
